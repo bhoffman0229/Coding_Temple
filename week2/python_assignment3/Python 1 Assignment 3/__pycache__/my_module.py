9e0e07aa17328278caa3caf8cf505062e4760c88
Brett Hoffman <bretthoffman44@gmail.com>
2023-07-20 11:24:54 -0400

    Add files via upload

---
+++ week2/python_assignment3/Python 1 Assignment 3/__pycache__/my_module.py
@@ -0,0 +1,6 @@
+def squarefoot(length, width: float):
+    return length*width
+def circumference(radius: float):
+    return 2*3.14159265359787*radius
+def feet_to_inch(feet: float):
+    return 12*feet
